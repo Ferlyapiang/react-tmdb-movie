5f905947ac7ea42b685935743f53ac7cabfe72f8
ferly.jeremi <ferly.jeremi@mncbank.co.id>
2024-04-24 15:17:50 +0700

    Update Middle.js

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,9 +1,39 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const MiddleView = ({ darkMode }) => {
-  const [isWatchlistPressed, setIsWatchlistPressed] = useState(false);
+  const [itemStatus, setItemStatus] = useState([
+    { key: 'Semua Film', pressed: false },
+    { key: 'XXI', pressed: false },
+    { key: 'CGV', pressed: false },
+    { key: 'Watchlist Saya', icon: 'heart-outline', pressed: false },
+  ]);
+
+  const renderItem = ({ item, index }) => (
+    <TouchableOpacity
+      style={[
+        styles.middleTextCircle,
+        { borderColor: item.pressed ? 'blue' : 'grey' },
+        { color: item.pressed ? 'blue' : (darkMode ? 'white' : 'black') }
+      ]}
+      onPress={() => {
+        const updatedStatus = [...itemStatus];
+        updatedStatus[index].pressed = !updatedStatus[index].pressed;
+        setItemStatus(updatedStatus);
+      }}
+    >
+      {item.icon && (
+        <Ionicons
+          name={item.icon}
+          style={{ marginRight: 5 }}
+          size={18}
+          color={darkMode ? 'white' : 'black'}
+        />
+      )}
+      <Text style={{ color: item.pressed ? 'blue' : (darkMode ? 'white' : 'black') }}>{item.key}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={[styles.middleContainer, { backgroundColor: darkMode ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0)' }]}>
@@ -14,52 +44,13 @@ const MiddleView = ({ darkMode }) => {
           <Ionicons name="chevron-forward-circle" size={24} color={darkMode ? 'white' : 'black'} />
         </View>
       </View>
-      
-      <View style={styles.middleRow}>
-      <TouchableOpacity
-          style={[
-            styles.middleTextCircle,
-            { borderColor: isWatchlistPressed ? 'red' : 'blue' },
-          ]}
-          onPress={() => setIsWatchlistPressed(!isWatchlistPressed)}
-        >
-        <Text>Semua Film</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.middleTextCircle,
-            { borderColor: isWatchlistPressed ? 'red' : 'blue' },
-          ]}
-          onPress={() => setIsWatchlistPressed(!isWatchlistPressed)}
-        >
-        <Text>XXI</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.middleTextCircle,
-            { borderColor: isWatchlistPressed ? 'red' : 'blue' },
-          ]}
-          onPress={() => setIsWatchlistPressed(!isWatchlistPressed)}
-        >
-        <Text>CGV</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity
-          style={[
-            styles.middleTextCircle,
-            { borderColor: isWatchlistPressed ? 'red' : 'blue' },
-          ]}
-          onPress={() => setIsWatchlistPressed(!isWatchlistPressed)}
-        >
-          <Ionicons
-            name="heart-outline"
-            style={{ marginRight: 5 }}
-            size={18}
-            color={darkMode ? 'white' : 'black'}
-          />
-          <Text>Watchlist Saya</Text>
-        </TouchableOpacity>
-      </View>
+      <FlatList
+        data={itemStatus}
+        renderItem={({ item, index }) => renderItem({ item, index, darkMode })}
+        keyExtractor={(item) => item.key}
+        contentContainerStyle={styles.flatListContainer}
+      />
+
     </View>
   );
 };
@@ -96,6 +87,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 20,
     padding: 5,
+    marginBottom: 20,
+  },
+  flatListContainer: {
+    flexDirection: 'row',
+    padding: 5
   },
 });
 
